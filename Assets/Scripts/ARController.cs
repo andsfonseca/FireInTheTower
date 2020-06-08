6357a56485f5024c15b35cf43409e788576ed577
andsfonseca <andsf@live.com>
2020-06-08 12:13:31 -0300

    fix: Agora só é possível escolher planos horizontais.
    * Adicionado API do google cloud anchor

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -29,13 +29,6 @@ public class ARController : MonoBehaviour {
         get { return _globalARSelected; }
     }
 
-
-    /// <summary>
-    /// A rotação em graus precisa ser aplicada ao objeto quando for colocada.
-    /// </summary>
-    private const float k_PrefabRotation = 180.0f;
-
-  
     /// <summary>
     /// The Unity Awake() method.
     /// </summary>
@@ -50,23 +43,22 @@ public class ARController : MonoBehaviour {
     /// </summary>
     public void Update() {
 
-        // If the player has not touched the screen, we are done with this update.
+        //Se a seleção está desabilitada não precisa começar a interface de seleção
+        if (!GameLogic.Instance.SelectOnTrackingPlanes) {
+            return;
+        }
+
+        // Se o player não tocou na tela, não precisa continuar
         Touch touch;
         if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began) {
             return;
         }
 
-        // Should not handle input if the player is pointing on UI.
+        // Se o player esta tocando na UI
         if (EventSystem.current.IsPointerOverGameObject(touch.fingerId)) {
             return;
         }
 
-        //Se a seleção está desabilitada, não precisar capturar os pontos selecionadas
-        if (!GameLogic.Instance.SelectOnTrackingPlanes) {
-            return;
-        }
-
-        // Raycast against the location the player touched to search for planes.
         TrackableHit hit;
         TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
             TrackableHitFlags.FeaturePointWithSurfaceNormal;
@@ -77,35 +69,38 @@ public class ARController : MonoBehaviour {
             if ((hit.Trackable is DetectedPlane) &&
                 Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
                     hit.Pose.rotation * Vector3.up) < 0) {
-                Debug.Log("Hit at back of the current DetectedPlane");
+                Debug.Log("O hit foi dado na parte de trás do plano");
             }
-            else {
+            else if (hit.Trackable is DetectedPlane) {
 
-                ///Caso a posição seja nula, cria o objeto
-                if (_globalARSelected == null) {
-                    _globalARSelected = new GameObject();
-                    _globalARSelected.name = "Global Position Marker";
-                    GameObject marker = Instantiate(_globalARSelected, Vector3.zero, Quaternion.identity, _globalARSelected.transform);
-                    marker.name = "Marker";
+                DetectedPlane detectedPlane = hit.Trackable as DetectedPlane;
 
+                //Se ele não estiver clicado em um plano na parede
+                if (detectedPlane.PlaneType != DetectedPlaneType.Vertical) {
+
+                    //Caso a posição seja nula, cria o objeto
+                    if (_globalARSelected == null) {
+                        _globalARSelected = new GameObject();
+                        _globalARSelected.name = "Global Position Marker";
+                        GameObject marker = Instantiate(_globalARSelected, Vector3.zero, Quaternion.identity, _globalARSelected.transform);
+                        marker.name = "Marker";
+                    }
+
+                    _globalARSelected.transform.position = hit.Pose.position;
+                    _globalARSelected.transform.rotation = hit.Pose.rotation;
+
+                    //Faz com que o objeto se rotacione para o lado do usuário, 
+                    _globalARSelected.transform.Rotate(0, 180, 0, Space.Self);
+
+                    // Cria uma âncora na posição selecioanda
+                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                    //Coloca o marcador como filho da Âncora
+                    _globalARSelected.transform.parent = anchor.transform;
+
+                    //Desabilita a seleção para um local
+                    GameLogic.Instance.SelectOnTrackingPlanes = false;
                 }
-
-                _globalARSelected.transform.position = hit.Pose.position;
-                _globalARSelected.transform.rotation = hit.Pose.rotation;
-
-                // Compensate for the hitPose rotation facing away from the raycast (i.e.
-                // camera).
-                _globalARSelected.transform.Rotate(0, k_PrefabRotation, 0, Space.Self);
-
-                // Create an anchor to allow ARCore to track the hitpoint as understanding of
-                // the physical world evolves.
-                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-
-                // Make game object a child of the anchor.
-                _globalARSelected.transform.parent = anchor.transform;
-
-                //Desabilita a seleção para um local
-                GameLogic.Instance.SelectOnTrackingPlanes = false;
 
             }
         }
